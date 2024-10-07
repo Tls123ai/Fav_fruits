062d3ba1d0f7651120597131454a4a6e2984d91d
Tls123ai <harryragot@gmail.com>
2024-10-07 08:55:06 +0800

    Update Task2.py

---
+++ Task2.py
@@ -0,0 +1,10 @@
+list_of_fruits = ["banana", "apple", "orange"]
+for f in list_of_fruits:
+    if f == "banana":
+        print("hellow, banana!")
+        
+    elif f == "apple":
+        print("hellow, apple!")
+    
+    else:
+        print("no")
